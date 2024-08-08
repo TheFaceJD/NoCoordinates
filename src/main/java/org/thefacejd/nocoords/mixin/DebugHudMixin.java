af60760d1863646f3ff03bc466896c2cb1c67ccd
thefaсejd <thefacejd@mail.ru>
2024-08-09 02:07:54 +0500

    Version 1.2

--- src/main/java/org/thefacejd/nocoords/mixin/DebugHudMixin.java
+++ src/main/java/org/thefacejd/nocoords/mixin/DebugHudMixin.java
@@ -11,12 +11,22 @@ import java.util.List;
 @Mixin(DebugHud.class)
 public class DebugHudMixin {
     @Inject(method = "getLeftText", at = @At("RETURN"), cancellable = true)
-    private void removeLines(CallbackInfoReturnable<List<String>> cir) {
+    private void removeLeftLines(CallbackInfoReturnable<List<String>> cir) {
         List<String> debugInfo = cir.getReturnValue();
         debugInfo.removeIf(line -> line.contains("XYZ:"));
         debugInfo.removeIf(line -> line.contains("Block:"));
         debugInfo.removeIf(line -> line.contains("Chunk:"));
         debugInfo.removeIf(line -> line.contains("Facing:"));
+        debugInfo.removeIf(line -> line.contains("Chunk-relative:"));
+        debugInfo.removeIf(line -> line.contains("CH") && line.contains("SH"));
+        cir.setReturnValue(debugInfo);
+    }
+
+    @Inject(method = "getRightText", at = @At("RETURN"), cancellable = true)
+    private void removeRightLines(CallbackInfoReturnable<List<String>> cir) {
+        List<String> debugInfo = cir.getReturnValue();
+        debugInfo.removeIf(line -> line.contains("Targeted Block:"));
+        debugInfo.removeIf(line -> line.contains("Targeted Fluid:"));
         cir.setReturnValue(debugInfo);
     }
 }
--- src/main/java/org/thefacejd/nocoords/mixin/MinecraftClientMixin.java
+++ src/main/java/org/thefacejd/nocoords/mixin/MinecraftClientMixin.java
@@ -2,7 +2,9 @@ package org.thefacejd.nocoords.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,14 +15,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
-
     @Inject(method = "tick", at = @At("TAIL"))
     private void showCoordinates(CallbackInfo ci) {
         ClientPlayerEntity player = MinecraftClient.getInstance().player;
-        if (player != null && (player.getWorld().getRegistryKey().equals(World.OVERWORLD) && player.getMainHandStack().getItem().equals(Items.COMPASS)
-                || !player.getWorld().getRegistryKey().equals(World.OVERWORLD) && player.getMainHandStack().getItem().equals(Items.RECOVERY_COMPASS))) {
+        if (player != null && (player.getWorld().getRegistryKey().equals(World.OVERWORLD) && onHands(player, Items.COMPASS)
+                || !player.getWorld().getRegistryKey().equals(World.OVERWORLD) && onHands(player, Items.RECOVERY_COMPASS))) {
             updateActionBar("%s %s %s %s".formatted(
-                    player.getHorizontalFacing().toString().substring(0, 1).toUpperCase() + player.getHorizontalFacing().toString().substring(1),
+                    getSide(player).getString(),
                     player.getBlockX(), player.getBlockY(), player.getBlockZ()));
         }
     }
@@ -28,5 +29,15 @@ public class MinecraftClientMixin {
     @Unique
     private void updateActionBar(String message) {
         MinecraftClient.getInstance().inGameHud.setOverlayMessage(Text.literal(message), false);
+    }
+
+    @Unique
+    private boolean onHands(ClientPlayerEntity player, Item item) {
+        return player.getMainHandStack().getItem().equals(item) || player.getOffHandStack().getItem().equals(item);
+    }
+
+    @Unique
+    private MutableText getSide(ClientPlayerEntity player) {
+        return Text.translatable("sides." + player.getHorizontalFacing().toString());
     }
 }
